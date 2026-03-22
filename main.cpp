adf23a659e6c80e780db40c0c52313faffe5d7ff
IrisSkledar <iris.skledar@student.um.si>
2026-03-22 17:36:17 +0100

    Feature input/output dodana funkcija Izpis_Stevil

--- main.cpp
+++ main.cpp
@@ -22,3 +22,14 @@ bool Branje_Stevil(vector<int> &vec, const char s[]) {
     input.close();
     return true;
 }
+
+void Izpis_Stevil(int* polje, unsigned int velikost) {
+    ofstream output("out.txt");
+
+    for (unsigned int i = 0; i < velikost; i++) {
+        output << polje[i];
+        if (i < velikost - 1) output << ' ';
+    }
+
+    output << endl;
+}

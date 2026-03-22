2034358438f312eb351d0cecb921a393922a91f5
IrisSkledar <iris.skledar@student.um.si>
2026-03-22 17:33:09 +0100

    Feature input/output dodana funkcija Branje_Stevil

--- main.cpp
+++ main.cpp
@@ -4,3 +4,21 @@
 #include <sstream>
 
 using namespace std;
+
+bool Branje_Stevil(vector<int> &vec, const char s[]) {
+    ifstream input(s);
+    int st;
+
+    if (!input.is_open()) {
+        return false;
+    }
+
+    while (!input.eof()) {
+        input >> st;
+        vec.push_back(st);
+        while (isspace(input.peek())) input.get();
+    }
+
+    input.close();
+    return true;
+}
